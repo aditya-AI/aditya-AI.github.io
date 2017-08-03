8624281423f2d93d4d7162149745af95150170ae
aditya101993 <adityasharma101993@gmail.com>
2017-08-04 04:44:35 +0530

    aa

--- build/files.conf.js
+++ build/files.conf.js
@@ -6,7 +6,8 @@ module.exports = {
         type: 'js',
         list: [
             'node_modules/jquery/dist/jquery.js',
-            'node_modules/bootstrap/dist/js/bootstrap.js'
+            'node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/pace-progress/pace.js'
         ]
     }, {
         name: 'index page',
@@ -17,8 +18,10 @@ module.exports = {
             'node_modules/jquery/dist/jquery.js',
             'node_modules/bootstrap/dist/js/bootstrap.js',
             'node_modules/chart.js/dist/Chart.js',
+            'node_modules/pace-progress/pace.js',
             'node_modules/wowjs/dist/wow.js',
-            'static/js/scroll.js'
+            'static/js/scroll.js',
+            'static/js/count.js'
         ]
     }, {
         name: 'i18next page',
@@ -38,9 +41,13 @@ module.exports = {
             'node_modules/jquery/dist/jquery.js',
             'search/js/bootstrap3-typeahead.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/metismenu/dist/metisMenu.js',
             'node_modules/jquery-slimscroll/jquery.slimscroll.js',
+            'node_modules/peity/jquery.peity.js',
+            'node_modules/pace-progress/pace.js',
             'node_modules/wowjs/dist/wow.js',
-            'static/js/scroll.js'
+            'static/js/scroll.js',
+            'static/js/count.js'
         ]
     }, {
         name: 'blog pages\' jPage',
@@ -48,7 +55,8 @@ module.exports = {
         prefix: 'jPage-',
         type: 'js',
         list: [
-            'static/js/jPages.js'
+            'static/js/jPages.js',
+            'static/js/js.js'
         ]
     }, {
         name: 'all pages',
@@ -61,7 +69,11 @@ module.exports = {
             'node_modules/animate.css/animate.css',
             'node_modules/components-font-awesome/css/font-awesome.css',
             'node_modules/font-mfizz/dist/font-mfizz.css',
+            'node_modules/gritter/jquery.gritter.css',
+            'node_modules/pace-progress/themes/blue/pace-theme-flash.css',
             'search/css/cb-search.css',
+            'static/css/pygments.css',
+            // 'static/css/style.css'
             'static/css/style-purify.css'
         ]
     }]
